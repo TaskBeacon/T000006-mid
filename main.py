d555347b6786173971d3c5b9fbab47c6e2ebd426
zh1peng <frueddy@gmail.com>
2025-06-21 21:40:57 +0800

    update

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ settings.add_subinfo(subject_data)
 
 # 4. setup triggers
 settings.triggers = cfg['trigger_config']
-# 4. setup triggers
-settings.triggers = cfg['trigger_config']
 
 ser = serial.serial_for_url("loop://", baudrate=115200, timeout=1)
 # ser = serial.Serial("COM3", baudrate=115200, timeout=1)
